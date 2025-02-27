0e57add020a1dbc7fc70b1c823998d60f5108b91
EC2 Default User <ec2-user@ip-172-31-45-168.ap-northeast-2.compute.internal>
2025-02-27 07:13:40 +0000

    d

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://nd3iefnod1.execute-api.ap-northeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://w400esnj5l.execute-api.ap-northeast-2.amazonaws.com/asd' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
